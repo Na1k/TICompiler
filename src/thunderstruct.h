e8349934119c929a2866db1b20e626144646bc06
firomaeor <firomaeor@gmx.de>
2021-06-04 19:14:30 +0200

    all kind of stuff again mostly tree generation and inline parsing does not quite work tho...:(

--- src/thunderstruct.h
+++ src/thunderstruct.h
@@ -1,59 +1,65 @@
 #ifndef THUNDERSTRUCT_H
 #define THUNDERSTRUCT_H
 
-        typedef enum Type {
-                INT = 0,
-                CHAR,
-                BOOL,
-                FLOAT,
-                STRING,
-                VARIABLE,
-                ARRAY
-        } Type;
+typedef enum Type {
+        INT = 0,
+        CHAR,
+        BOOL,
+        FLOAT,
+        STRING,
+        VARIABLE,
+        ARRAY
+} Type;
 
-        typedef enum Flags {
-                E_CONST = 0x01,         //0001
-                E_VAR = 0x02,           //0010
-                E_ARR = 0x04,           //0100
-                E_UNDEF = 0x08          //1000
-        } Flags;
+typedef enum Flags {
+        E_CONST = 0x01,         //0001
+        E_VAR = 0x02,           //0010
+        E_ARR = 0x04,           //0100
+        E_UNDEF = 0x08          //1000
+} Flags;
 
-        typedef enum NodeType {
-                E_OPERATION = 0,
-                E_VALUE
-        } NodeType;
+typedef enum NodeType {
+        E_OPERATION = 0,
+        E_VALUE
+} NodeType;
 
-        typedef struct SyntaxNode {
-                int nodeType;               //identifiziert die node selbst (eg Int/eqlvl1 etc...)
-                int valueType;
-                union
-                {
-                        int ival;
-                        float fval;
-                        char *sval;
-                };
-                struct SyntaxNode* leftChild;
-                struct SyntaxNode* rightChild;
-        } SyntaxNode;
-
-        typedef struct Variable {
-                char* name;
-                Type type;
-                Flags flags;
-                SyntaxNode* value;              //zeigt auf SyntaxNode
-                int length;                     //wie viele Bytes?
-                struct Variable* next;
-        } Variable;
-
-        typedef struct Data
+typedef struct SyntaxNode {
+        int nodeType;               //identifiziert die node selbst (eg Int/eqlvl1 etc...)
+        int valueType;
+        int expressionType;
+        union
         {
-                int type;
-                union
-                {
-                        int ival;
-                        float fval;
-                        char *sval;
-                }; 
-        } Data;
+                int ival;
+                float fval;
+                char *sval;
+        };
+        struct SyntaxNode* leftChild;
+        struct SyntaxNode* rightChild;
+} SyntaxNode;
+
+typedef struct NodeQueue {
+        SyntaxNode* node;
+        struct NodeQueue* next;
+} NodeQueue;
+
+typedef struct Variable {
+        char* name;
+        Type type;
+        Flags flags;
+        SyntaxNode* value;              //zeigt auf SyntaxNode
+        int length;                     //wie viele Bytes?
+        struct Variable* next;
+} Variable;
+
+typedef struct Data
+{
+        int type;
+        union
+        {
+                int ival;
+                float fval;
+                char *sval;
+        };
+} Data;
 
 #endif //THUNDERSTRUCT_H
