f764ee462947f4995a51d56171b64a76b7f704c6
firomaeor <firomaeor@gmx.de>
2021-06-10 20:45:06 +0200

    final fixes to arrays

--- src/CompileYaccLex.sh
+++ src/CompileYaccLex.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: CompileYaccLex.sh <yacc>.y <lex>.l <progname>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: CompileYaccLex.sh <yacc>.y <lex>.l"
     exit
 fi
 
 yacc -vd --debug --verbose $1
 lex $2
 
-gcc y.tab.c lex.yy.c -std=c11 -Wall -o $3 -ll -g
+gcc y.tab.c lex.yy.c -o iguanacompiler -ll -g
 
 rm lex.yy.* y.tab.* y.output
---
+++ src/install.sh
@@ -0,0 +1,1 @@
+cp iguanacompiler /usr/bin/iguanacompiler
